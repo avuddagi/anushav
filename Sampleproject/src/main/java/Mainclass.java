c40ce91c4614bad677696bba53954504985ec30a
avuddagi <avuddagi@DIN73002121.corp.capgemini.com>
2019-07-04 15:30:46 +0530

    mai

--- Sampleproject/src/main/java/Mainclass.java
+++ Sampleproject/src/main/java/Mainclass.java
@@ -1,7 +1,8 @@
 
 public class Mainclass { 
 	    public static void main(String args[]){  
-	     System.out.println("Hello Java");  
+	     System.out.println("Hello Java");
+	     System.out.println("Hello world");  
 	    }  
 	}  
 
